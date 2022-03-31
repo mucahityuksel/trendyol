1ec535ae073a31897f4b25b1675cfab979b40fe1
mucahityuksel <m.yuksel793@gmail.com>
2022-03-31 21:26:43 +0300

    created project

---
+++ src/myroute.tsx
@@ -0,0 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./Pages/Home";
+
+function MyRoute() {
+  return <Router>
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+    </Switch>
+  </Router>
+
+}
+
+export default MyRoute
