c36574e098ed27ccdb0aea26dfffb7e327112cf5
mucahityuksel <m.yuksel793@gmail.com>
2022-04-22 23:51:45 +0300

    favorite header finished

--- src/Components/FavoritesHeader/index.tsx
+++ src/Components/FavoritesHeader/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BsFillBookmarkFill } from 'react-icons/bs'
 import { FiSearch } from 'react-icons/fi'
 import { MdOutlineFavorite } from 'react-icons/md'
 import "./style.scss"
@@ -13,12 +14,16 @@ function FavoritesHeader() {
                         <MdOutlineFavorite size={"1.5em"} color="#f27a1a"></MdOutlineFavorite>
                         <span>Favorilerim</span>
                     </div>
+                    <div className='favorite-bookmark'>
+                        <BsFillBookmarkFill size={"1.5em"} ></BsFillBookmarkFill>
+                        <span>Koleksiyonlarım</span>
+                    </div>
                 </div>
                 <div className='favorite-header-right'>
                     <div className='search-comp'>
                         <div className='header-search-bar'>
                             <div className="header-search">
-                                <input className='search-input' placeholder='Aradığınız ürün, ktegori veya markayı yazınız'></input>
+                                <input className='search-input' placeholder='Favorilerimde ara'></input>
                                 <FiSearch className='search-icon'></FiSearch>
                             </div>
                         </div>
